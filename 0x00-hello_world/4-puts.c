f6812ec61f4122f9ab03949274211db8aadf663f
codezforhire <anassmrizig@gmail.com>
2023-07-13 07:09:56 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
+ *  main -Entry point
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-    /*
-     * The program is used to print the desired message
-     * "Programming is like building a multilingual puzzle"
-     * followed by a new line.
-     */
-    puts("Programming is like building a multilingual puzzle");
-
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

0be21374de9817ca50c53aae982ee600bd279605
codezforhire <anassmrizig@gmail.com>
2023-07-14 08:23:31 -0700

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible different combinations of three digits.
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int num1, num2, num3;
+	int num1, num2, num3;
 
-    for (num1 = 0; num1 <= 7; num1++)
-    {
-        for (num2 = num1 + 1; num2 <= 8; num2++)
-        {
-            for (num3 = num2 + 1; num3 <= 9; num3++)
-            {
-                putchar((num1 % 10) + '0');
-                putchar((num2 % 10) + '0');
-                putchar((num3 % 10) + '0');
+	for (num1 = 0; num1 <= 7; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 8; num2++)
+		{
+			for (num3 = num2 + 1; num3 <= 9; num3++)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
 
-                if (num1 == 7 && num2 == 8 && num3 == 9)
-                    continue;
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
